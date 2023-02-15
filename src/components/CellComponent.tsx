124fda4209ea4e7b1790ae64f509c624ddde61dd
Oleksandr Kuznietsov <funhardmode@gmail.com>
2023-02-15 21:42:27 +0200

    Created new clases for Figures, The figures are arranged on the board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -7,7 +7,7 @@ interface CellProps {
 export const CellComponent: FC<CellProps> = ({cell}) => {
     return (
         <div className={['cell', cell.color].join(' ')}>
-
+            {cell.figure?.logo && <img src={cell.figure.logo} alt =""/>}
         </div>
     );
 };
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,8 +1,15 @@
 import { Cell } from "./Cell"
 import { Colors } from "./Colors";
+import { Queen } from "./figures/Queen";
+import { Pawn } from "./figures/Pawn";
+import { King } from "./figures/King";
+import { Knight } from "./figures/Knight";
+import { Bishop } from "./figures/Bishop";
+import { Rook } from "./figures/Rook";
 
 export class Board {
-    cells: Cell[][] = []
+    cells: Cell[][] = [];
+    
 
     public initCells() {
         for (let i = 0; i < 8; i++) {
@@ -16,5 +23,58 @@ export class Board {
             }
             this.cells.push(row);
         }
+    }
+
+    public getCell (x:number, y: number) {
+        return this.cells[y][x];
+    }
+
+    private addPawns () {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1) )
+        }
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.WHITE, this.getCell(i, 6) )
+        }
+    }
+
+    private addKings() {
+        new King (Colors.WHITE, this.getCell(4,7))
+        new King (Colors.BLACK, this.getCell(4,0)) 
+    }
+
+    private addQueens() {
+        new Queen (Colors.WHITE, this.getCell(3,7))
+        new Queen (Colors.BLACK, this.getCell(3,0)) 
+    }
+
+    private addKnights() {
+        new Knight (Colors.WHITE, this.getCell(1,7))
+        new Knight (Colors.WHITE, this.getCell(6,7))
+        new Knight (Colors.BLACK, this.getCell(1,0)) 
+        new Knight (Colors.BLACK, this.getCell(6,0)) 
+    }
+
+    private addBishops() {
+        new Bishop (Colors.WHITE, this.getCell(2,7))
+        new Bishop (Colors.WHITE, this.getCell(5,7))
+        new Bishop (Colors.BLACK, this.getCell(2,0)) 
+        new Bishop (Colors.BLACK, this.getCell(5,0)) 
+    }
+
+    private addRooks () {
+        new Rook (Colors.WHITE, this.getCell(0,7))
+        new Rook (Colors.WHITE, this.getCell(7,7))
+        new Rook (Colors.BLACK, this.getCell(0,0)) 
+        new Rook (Colors.BLACK, this.getCell(7,0)) 
+    }
+
+    public addFigures() {
+        this.addPawns();
+        this.addBishops();
+        this.addKings();
+        this.addKnights();
+        this.addQueens();
+        this.addRooks();
     }
 }
