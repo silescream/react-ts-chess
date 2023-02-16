6d6314955446c61920e6592565df431f00e12580
Oleksandr Kuznietsov <funhardmode@gmail.com>
2023-02-16 15:15:51 +0200

    added functionality to select cell

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
 import { CellComponent } from "./CellComponent";
 
 
@@ -8,18 +9,27 @@ interface BoardProps {
     setBoard: (board: Board) => void;
 }
 const BoardComponent: FC<BoardProps>  = ({board, setBoard}) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+    function click (cell: Cell) {
+      if (cell.figure) {
+        setSelectedCell(cell);
+      }
+    }
     return (
     <div className="board">
     {board.cells.map((row, index) =>
       <React.Fragment key={index}>
         {row.map(cell =>
           <CellComponent 
+          click = {click}
           cell ={cell}
           key={cell.id}
+          selected = {cell.x === selectedCell?.x && cell.y === selectedCell?.y}
           />
         )}
       </React.Fragment>
-        )};
+        )}
     </div> 
     );
 };    
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,10 +3,14 @@ import { Cell } from '../models/Cell';
 
 interface CellProps {
     cell: Cell;
+    selected: boolean;
+    click: (cell:Cell) => void;
 }
-export const CellComponent: FC<CellProps> = ({cell}) => {
+export const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
-        <div className={['cell', cell.color].join(' ')}>
+        <div className={['cell', cell.color, selected ? "selected" : ''].join(' ')}
+             onClick = {() => click(cell)}>
+            <div className = {"available"} />
             {cell.figure?.logo && <img src={cell.figure.logo} alt =""/>}
         </div>
     );
